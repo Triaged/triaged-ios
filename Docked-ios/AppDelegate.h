a3efbc156433815bfeac0465418687df620c936b
Charlie White <charlie@anchor.io>
2013-09-19 09:27:18 -0700

    Initial Commit

---
+++ Docked-ios/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Docked-ios
+//
+//  Created by Charlie White on 9/19/13.
+//  Copyright (c) 2013 Charlie White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
